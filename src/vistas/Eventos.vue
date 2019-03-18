5961e03633d0133c6351a9e929c006b16b429262
Maria Laura Rojas <47549744+LaMariaDeBellaSuiza@users.noreply.github.com>
2019-03-18 04:26:34 +0100

    nuevas tarjetas en eventos

--- src/vistas/Eventos.vue
+++ src/vistas/Eventos.vue
@@ -17,16 +17,14 @@
             <b-nav-item :to="{name:'buscar'}" size="sm" class="my-2 my-sm-0">
             <i class="fas fa-search"></i>
             </b-nav-item>
-        </b-nav-form>
-        
+        </b-nav-form>    
       </b-navbar-nav>
-    </b-collapse>
   </b-navbar>
-</div>
-<div>
+  <div>
+  <b-card-group deck>
   <b-card
-    title="Card Title"
-    img-src="https://picsum.photos/600/300/?image=25"
+    title="Bodas"
+    img-src="http://memoflores.com/fotos-de-bodas-01.jpg"
     img-alt="Image"
     img-top
     tag="article"
@@ -34,10 +32,53 @@
     class="mb-2"
   >
     <b-card-text>
-      Some quick example text to build on the card title and make up the bulk of the card's content.
+      Casa Logieventos quiere hacer parte de la boda de tus sueños. Ese momento en el que todo el mundo te mira con sonrisas y lágrimas; es uno de los mejores recuerdos que tendrás.
     </b-card-text>
-
-    <b-button href="#" variant="primary">Go somewhere</b-button>
+    <b-button href="#" variant="primary">See more</b-button>
   </b-card>
+  <b-card
+    title="15 Años"
+    img-src="http://www.fiestade15.com/ideas/imagenes/cake_topper_para_pastel_de_15_anos_1.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Ya que cumplir 15 años es algo inolvidable, Casa Logieventos quiere hacer parte de ese día tan especial para ti y brindarte tus servicios en eventos, para que tu fiesta sea tan especial como tu lo soñaste... 
+    </b-card-text>
+    <b-button href="#" variant="primary">See more</b-button>
+  </b-card>
+  <b-card
+    title="Fiestas Infantiles"
+    img-src="https://www.somosmamas.com.ar/wp-content/uploads/2018/11/juegos-para-fiestas-infantiles.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+      Nuestas fiestas son únicas y variadas, poseemos diseños temáticos especiales que incluyen decoración alusiva al tema de la fiesta.  
+    </b-card-text>
+    <b-button href="#" variant="primary">See more</b-button>
+  </b-card>
+ <b-card
+    title="Primeras Comuniones"
+    img-src="http://www.fiestade15.com/ideas/imagenes/cake_topper_para_pastel_de_15_anos_1.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+    <b-card-text>
+     
+    </b-card-text>
+    <b-button href="#" variant="primary">See more</b-button>
+  </b-card>
+  </b-card-group>
+</div>
 </div>
 </template>
--- src/vistas/Galeria.vue
+++ src/vistas/Galeria.vue
@@ -1,5 +1,39 @@
 <template>
   <div>
-    <h1>Fotos</h1>
-  </div>
+ <b-navbar class="navbar fixed-top" toggleable="md" type="dark" variant="dark">
+      <b-navbar-toggle target="nav_collapse"/>
+    <b-navbar-toggle target="nav_collapse" />
+    <b-navbar-nav>
+    <b-navbar-brand :to="{name:'Inicio'}" id="logo"> Casa LogiEventos</b-navbar-brand>
+          <b-nav-item :to="{name:'Eventos'}" exact class="Letranavbar"> </b-nav-item>
+          <b-nav-item :to="{name:'Eventos'}" class="Letranavbar">Eventos</b-nav-item>
+          <b-nav-item :to="{name:'Galeria'}" class="Letranavbar">Galeria</b-nav-item>
+          <b-nav-item :to="{name:'Contacto'}" class="Letranavbar">Contacto</b-nav-item>
+        </b-navbar-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2 buscarcuadro" type="text" placeholder="Buscar"/>
+            <b-nav-item :to="{name:'buscar'}" size="sm" class="my-2 my-sm-0">
+            <i class="fas fa-search"></i>
+            </b-nav-item>
+        </b-nav-form>    
+      </b-navbar-nav>
+  </b-navbar>
+  <div>
+  <b-card
+    overlay
+    img-src="https://i.ytimg.com/vi/8LH2DgvJJQo/maxresdefault.jpg"
+    img-alt="Card Image"
+    text-variant="white"
+    title="Image Overlay"
+    sub-title="Subtitle"
+  >
+    <b-card-text>
+      Some quick example text to build on the card and make up the bulk of the card's content.
+    </b-card-text>
+  </b-card>
+</div>
+
+</div>
 </template>
