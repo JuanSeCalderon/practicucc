791bfce71d2f60d3404eab4f37872293a380c021
Maria Laura Rojas <47549744+LaMariaDeBellaSuiza@users.noreply.github.com>
2019-03-26 05:17:55 +0100

    algunos eventos

--- src/vistas/Eventos.vue
+++ src/vistas/Eventos.vue
@@ -1,8 +1,6 @@
 <template>
 <div>
-<template>
-  <b-breadcrumb :items="items" />
-</template>
+<b-breadcrumb :items="items" />
  <b-navbar class="navbar fixed-top" toggleable="md" type="dark" variant="dark">
       <b-navbar-toggle target="nav_collapse"/>
     <b-navbar-toggle target="nav_collapse" />
@@ -15,29 +13,34 @@
         </b-navbar-nav>
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
+         <b-collapse is-nav class="nav_collapse">
+            <a href="https://www.facebook.com/bodaslogieventos/" target="_blank"><img alt="FanPage Casa Logieventos" src="https://lh6.googleusercontent.com/-CYt37hfDnQ8/T3nNydojf_I/AAAAAAAAAr0/P5OtlZxV4rk/s32/facebook32.png" width=32 height=32  /></a>&nbsp;&nbsp;&nbsp;
+            <a href="https://www.instagram.com/logieventos/?hl=es-la" target="_blank"><img alt="Insta Casa Logieventos" src="https://lh5.googleusercontent.com/-2cd4qn_7yDs/UIqEfqncDaI/AAAAAAAABfw/qhUaWfiH7DU/s32/instagram32.png" width=32 height=32  /></a>&nbsp;&nbsp;&nbsp;
+            <a href="https://api.whatsapp.com/send?phone=573143182893&text=Hola,%20quisiera%20alquilar%20un%20servicio%20de%20eventos" target="_blank"><img alt="Whatsapp Casa Logieventos" src="../assets/WhatsApp.png" width=32 height=32  /></a>&nbsp;&nbsp;
         <b-nav-form>
           <b-form-input size="sm" class="mr-sm-2 buscarcuadro" type="text" placeholder="Buscar"/>
             <b-nav-item :to="{name:'buscar'}" size="sm" class="my-2 my-sm-0">
             <i class="fas fa-search"></i>
             </b-nav-item>
-        </b-nav-form>    
+        </b-nav-form>
+          </b-collapse>
       </b-navbar-nav>
   </b-navbar>
   <div>
   <b-card-group deck>
-  <b-card
+  <b-card 
     title="Bodas"
-    img-src="http://memoflores.com/fotos-de-bodas-01.jpg"
+    img-src="http://memoflores.com/fotos-de-bodas-01.jpg" 
     img-alt="Image"
     img-top
     tag="article"
-    style="max-width: 20rem;"
+    style="max-width: 500px;"
     class="mb-2"
   >
     <b-card-text>
       Casa Logieventos quiere hacer parte de la boda de tus sueños. Ese momento en el que todo el mundo te mira con sonrisas y lágrimas; es uno de los mejores recuerdos que tendrás.
     </b-card-text>
-    <b-button href="#" variant="primary">See more</b-button>
+    <b-button href="#" variant="primary">Ver Más</b-button>
   </b-card>
   <b-card
     title="15 Años"
@@ -45,43 +48,105 @@
     img-alt="Image"
     img-top
     tag="article"
-    style="max-width: 20rem;"
+    style="max-width: 500px;"
     class="mb-2"
   >
     <b-card-text>
       Ya que cumplir 15 años es algo inolvidable, Casa Logieventos quiere hacer parte de ese día tan especial para ti y brindarte tus servicios en eventos, para que tu fiesta sea tan especial como tu lo soñaste... 
     </b-card-text>
-    <b-button href="#" variant="primary">See more</b-button>
+    <b-button href="#" variant="primary">Ver Más</b-button>
   </b-card>
+  </b-card-group>
+  <b-card-group deck>
+  
   <b-card
     title="Fiestas Infantiles"
     img-src="https://www.somosmamas.com.ar/wp-content/uploads/2018/11/juegos-para-fiestas-infantiles.jpg"
     img-alt="Image"
     img-top
     tag="article"
-    style="max-width: 20rem;"
+    style="max-width: 500px;"
     class="mb-2"
   >
     <b-card-text>
       Nuestas fiestas son únicas y variadas, poseemos diseños temáticos especiales que incluyen decoración alusiva al tema de la fiesta.  
     </b-card-text>
-    <b-button href="#" variant="primary">See more</b-button>
+    <b-button href="#" variant="primary">Ver Más</b-button>
   </b-card>
- <b-card
+ <b-card 
     title="Primeras Comuniones"
-    img-src="http://www.fiestade15.com/ideas/imagenes/cake_topper_para_pastel_de_15_anos_1.jpg"
+    img-src="https://www.fomento.edu/pinoalbar/sites/default/files/styles/noticia_interior/public/dsc_5935.jpg?itok=RQD6_d-n"
     img-alt="Image"
     img-top
     tag="article"
-    style="max-width: 20rem;"
-    class="mb-2"
+    style="max-width: 500px;"
+    class="mb-2"  
+   
   >
     <b-card-text>
      
     </b-card-text>
-    <b-button href="#" variant="primary">See more</b-button>
+    <b-button href="#" variant="primary">Ver Más</b-button>
+  </b-card>
+  </b-card-group>
+  <b-card-group deck>
+  <b-card
+    title="Grados"
+    img-src="https://cordopolis.es/la-ultima-oportunidad/files/2018/04/master_universitario.jpg" 
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 500px;"
+    class="mb-2"
+  >
+    <b-card-text>
+      
+    </b-card-text>
+    <b-button href="#" variant="primary">Ver Más</b-button>
+  </b-card>
+  <b-card
+    title="Bautizos"
+    img-src="https://www.carolbravo.com/wp-content/uploads/Fotografos-bautizos-madrid-00.jpg"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 500px;"
+    class="mb-2"
+  >
+    <b-card-text>
+      
+    </b-card-text>
+    <b-button href="#" variant="primary">Ver Más</b-button>
   </b-card>
   </b-card-group>
 </div>
+
 </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          {
+            text: 'Casa LogiEventos',
+            href: '#'
+          },
+          {
+            text: 'Eventos',
+             active: true
+          },
+          {
+            text: 'Galeria',
+             href: '#'
+          },
+          {
+            text: 'Contacto',
+             href: '#'
+          }
+        ]
+      }
+    }
+  }
+  
+</script>
--- src/vistas/Galeria.vue
+++ src/vistas/Galeria.vue
@@ -1,5 +1,7 @@
 <template>
+
   <div>
+  <b-breadcrumb :items="items" />
  <b-navbar class="navbar fixed-top" toggleable="md" type="dark" variant="dark">
       <b-navbar-toggle target="nav_collapse"/>
     <b-navbar-toggle target="nav_collapse" />
@@ -12,28 +14,105 @@
         </b-navbar-nav>
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
+         <b-collapse is-nav class="nav_collapse">
+            <a href="https://www.facebook.com/bodaslogieventos/" target="_blank"><img alt="FanPage Casa Logieventos" src="https://lh6.googleusercontent.com/-CYt37hfDnQ8/T3nNydojf_I/AAAAAAAAAr0/P5OtlZxV4rk/s32/facebook32.png" width=32 height=32  /></a>&nbsp;&nbsp;&nbsp;
+            <a href="https://www.instagram.com/logieventos/?hl=es-la" target="_blank"><img alt="Insta Casa Logieventos" src="https://lh5.googleusercontent.com/-2cd4qn_7yDs/UIqEfqncDaI/AAAAAAAABfw/qhUaWfiH7DU/s32/instagram32.png" width=32 height=32  /></a>&nbsp;&nbsp;&nbsp;
+            <a href="https://api.whatsapp.com/send?phone=573143182893&text=Hola,%20quisiera%20alquilar%20un%20servicio%20de%20eventos" target="_blank"><img alt="Whatsapp Casa Logieventos" src="../assets/WhatsApp.png" width=32 height=32  /></a>&nbsp;&nbsp;
         <b-nav-form>
           <b-form-input size="sm" class="mr-sm-2 buscarcuadro" type="text" placeholder="Buscar"/>
             <b-nav-item :to="{name:'buscar'}" size="sm" class="my-2 my-sm-0">
             <i class="fas fa-search"></i>
             </b-nav-item>
         </b-nav-form>    
+        </b-collapse>
       </b-navbar-nav>
   </b-navbar>
   <div>
-  <b-card
+  <b-card 
     overlay
     img-src="https://i.ytimg.com/vi/8LH2DgvJJQo/maxresdefault.jpg"
     img-alt="Card Image"
-    text-variant="white"
-    title="Image Overlay"
-    sub-title="Subtitle"
+    
   >
-    <b-card-text>
-      Some quick example text to build on the card and make up the bulk of the card's content.
-    </b-card-text>
+       <b-button variant="secondary">15 Años</b-button>
   </b-card>
 </div>
-
+ <div>
+  <b-card 
+    overlay
+    img-src="http://maneltamayo.com/wp-content/uploads/2015/11/boda-michelle-bradley-novios-pose-manel-tamayo-fotogrago-bodas.jpg"
+    img-alt="Card Image"
+    
+  >
+      <b-button variant="secondary">Bodas</b-button>
+  </b-card>
+</div>
+ <div>
+  <b-card 
+    overlay
+    img-src="http://www.revistacompensar.com/wp-content/uploads/2018/02/15-de-febrero-10.jpg"
+    img-alt="Card Image"
+    
+  >
+      <b-button variant="secondary">Fiestas Infantiles</b-button>
+  </b-card>
+</div>
+<div>
+  <b-card 
+    overlay
+    img-src="https://colegiomayordelosandes.edu.co/wp-content/uploads/2018/03/primeras-comuniones-cma3-1024x683.jpg"
+    img-alt="Card Image"
+    
+  >
+      <b-button variant="secondary">Primeras Comuniones</b-button>
+  </b-card>
+</div>
+<div>
+  <b-card 
+    overlay
+    img-src="https://elduendelibrecom.files.wordpress.com/2018/03/universidad-gratis.jpeg?w=950"
+    img-alt="Card Image"
+    
+  >
+      <b-button variant="secondary">Grados</b-button>
+  </b-card>
+</div>
+<div>
+  <b-card 
+    overlay
+    img-src="https://www.violetarodriguez.com/images/com_osgallery/gal-13/original/fotos-bautizo-madrid-021B349321-982C-882E-B8C8-1A457BAC2C3C.jpg"
+    img-alt="Card Image"
+    
+  >
+      <b-button variant="secondary">Bautizos</b-button>
+  </b-card>
+</div>
 </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          {
+            text: 'Casa LogiEventos',
+            href: '#'
+          },
+          {
+            text: 'Eventos',
+            href: '#'
+          },
+          {
+            text: 'Galeria',
+            active: true    
+          },
+          {
+            text: 'Contacto',
+             href: '#'
+          }
+        ]
+      }
+    }
+  }
+  
+</script>
