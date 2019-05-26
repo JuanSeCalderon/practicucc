451dd208f6537f3fc7d2ffbdf14026e58d4b1c4e
Maria Laura Rojas <47549744+LaMariaDeBellaSuiza@users.noreply.github.com>
2019-05-26 06:31:23 +0200

    Galeria

--- src/vistas/Galeria.vue
+++ src/vistas/Galeria.vue
@@ -1,68 +1,333 @@
 <template>
-
   <div>
+ <head>
+    <title>Galeria</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css" />
+    <link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+</head>
+<body>
+ <h1>Galeria</h1>
+  <!--Conteudo-->
   <div class="container">
-  <b-card 
-    overlay
-    img-src="https://i.ytimg.com/vi/8LH2DgvJJQo/maxresdefault.jpg"
-    img-alt="Card Image"
+  <h2>15 Años</h2>
+  <section class="galeria">
+    <div class="cont_accordion" data-accordion="1">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    <div class="cont_accordion" data-accordion="2">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
     
-  >
-       <b-button variant="secondary">15 Años</b-button>
-  </b-card>
-</div>
- <div class="container">
-  <b-card 
-    overlay
-    img-src="http://maneltamayo.com/wp-content/uploads/2015/11/boda-michelle-bradley-novios-pose-manel-tamayo-fotogrago-bodas.jpg"
-    img-alt="Card Image"npm i vue-glide-js
+    <div class="cont_accordion" data-accordion="3">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
     
-  >
-      <b-button variant="secondary">Bodas</b-button>
-  </b-card>
-</div>
- <div class="container">
-  <b-card 
-    overlay
-    img-src="http://www.revistacompensar.com/wp-content/uploads/2018/02/15-de-febrero-10.jpg"
-    img-alt="Card Image"
+    <div class="cont_accordion" data-accordion="4">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
     
-  >
-      <b-button variant="secondary">Fiestas Infantiles</b-button>
-  </b-card>
-</div>
-<div class="container">
-  <b-card 
-    overlay
-    img-src="https://colegiomayordelosandes.edu.co/wp-content/uploads/2018/03/primeras-comuniones-cma3-1024x683.jpg"
-    img-alt="Card Image"
+    <div class="cont_accordion" data-accordion="5">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
     
-  >
-      <b-button variant="secondary">Primeras Comuniones</b-button>
-  </b-card>
-</div>
-<div class="container">
-  <b-card 
-    overlay
-    img-src="https://elduendelibrecom.files.wordpress.com/2018/03/universidad-gratis.jpeg?w=950"
-    img-alt="Card Image"
-    
-    
-  >
-      <b-button variant="secondary">Grados</b-button>
-  </b-card>
-</div>
-<div class="container">
-  <b-card 
-    overlay
-    img-src="https://www.violetarodriguez.com/images/com_osgallery/gal-13/original/fotos-bautizo-madrid-021B349321-982C-882E-B8C8-1A457BAC2C3C.jpg"
-    img-alt="Card Image"
-    
-  >
-      <b-button variant="secondary">Bautizos</b-button>
-  </b-card>
-</div>
+     <div class="cont_accordion" data-accordion="6">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+
+  </section>  
+  </div>
  
+ <div class="container">
+  <h2>Bodas</h2>
+  <section class="galeria">
+    <div class="cont_accordion" data-accordion="1.1">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    <div class="cont_accordion" data-accordion="2.2">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="3.3">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="4.4">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="5.5">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+     <div class="cont_accordion" data-accordion="6.6">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+
+  </section>  
+  </div>
+
+<div class="container">
+  <h2>Fiestas infantiles</h2>
+  <section class="galeria">
+    <div class="cont_accordion" data-accordion="1.1.1">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    <div class="cont_accordion" data-accordion="2.2.2">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="3.3.3">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="4.4.4">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="5.5.5">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+     <div class="cont_accordion" data-accordion="6.6.6">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+
+  </section>  
+  </div>
+
+<div class="container">
+  <h2>Primeras comuniones</h2>
+  <section class="galeria">
+    <div class="cont_accordion" data-accordion="1.1.1.1">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    <div class="cont_accordion" data-accordion="2.2.2.2">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="3.3.3.3">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="4.4.4.4">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="5.5.5.5">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+     <div class="cont_accordion" data-accordion="6.6.6.6">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+
+  </section>  
+  </div>
+
+<div class="container">
+  <h2>Grados</h2>
+  <section class="galeria">
+    <div class="cont_accordion" data-accordion="1.1.1.1.1">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    <div class="cont_accordion" data-accordion="2.2.2.2.2">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="3.3.3.3.3">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="4.4.4.4.4">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="5.5.5.5.5">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+     <div class="cont_accordion" data-accordion="6.6.6.6.6">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+
+  </section>  
+  </div>
+
+  <div class="container">
+  <h2>Bautizos</h2>
+  <section class="galeria">
+    <div class="cont_accordion" data-accordion="1.1.1.1.1.1">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    <div class="cont_accordion" data-accordion="2.2.2.2.2.2">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="3.3.3.3.3.3">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="4.4.4.4.4.4">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+    <div class="cont_accordion" data-accordion="5.5.5.5.5.5">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+    
+     <div class="cont_accordion" data-accordion="6.6.6.6.6.6">
+      <div class="cont_inside">
+        <div class="cont_caption">
+          <h1></h1>
+        </div>
+      </div>
+    </div>
+
+  </section>  
+  </div>
+
+</body>
 
 </div>
 </template>
@@ -71,12 +336,268 @@
 </script>
 
 <style scoped >
-.container {
-  padding: 40px 80px;
-  position: relative;
+body {
+  background: #fff;
+    font-family: 'Courgette', cursive;
+
+}
+.container{
+  justify-content: center;
+}
+.galeria {
+  padding: 20px ;
+  width: 80vw;
+  height: 80vh;
   display: flex;
-  flex-wrap: wrap;
- justify-content: center;
+  flex-direction: row;
+  justify-content: stretch;
+}
+
+h1 {
+  margin: 100px;
+    font-size: 50px;
+    font-weight: 700;
+    margin-bottom: 80px;
+    text-align: center;
+}
+
+.cont_accordion {
+  flex: 1;
+  transition: 0.4s ease-in-out;
+  -webkit-transform: skewX(-5deg);
+          transform: skewX(-5deg);
+  overflow: hidden;
+  display: flex;
+  border-left: 4px solid #fff;
+  /* -webkit-filter: grayscale(100%);
+           filter: grayscale(100%); Para criar um filtro cinza*/
+}
+[data-accordion].cont_accordion:hover,
+[data-accordion].cont_accordion:focus {
+  flex: 20; /*Determina a largura da imagem*/
+/*    -webkit-filter: grayscale(80%);
+          filter: grayscale(80%);  Cria filtro cinza*/
+}
+.cont_accordion:first-child {
+  border-left: 0;
+  flex: 1.25;
+  margin-left: -3em;
+}
+.cont_accordion:last-child {
+  flex: 1.25;
+  margin-right: -3em;
+}
+.cont_inside {
+  -webkit-transform: skewX(5deg);
+          transform: skewX(5deg);
+  padding: 4em;
+  background-cover: cover;
+  background-position: 50% 50%;
+  margin: 0 -3em;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.cont_inside .cont_caption {display:none;}
+.cont_inside:hover .cont_caption {display:block;}
+
+[data-accordion="1"] .cont_inside {
+  background-image: url("https://coolmusic.com.co/wp-content/uploads/2019/01/11-fiestas-de-15-anos-coolmusic-1.jpg");
+  background-position: 0 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="2"] .cont_inside {
+  background-image: url("https://coolmusic.com.co/wp-content/uploads/2019/01/2-fiestas-de-15-anos-coolmusic.jpg");
+  background-position: 27.85% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="3"] .cont_inside {
+  background-image: url("http://www.eventoschampagnat.com/uploads/images/IMG_2.JPG");
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="4"] .cont_inside {
+  background-image: url("http://www.espacioamaryllis.com.ar/wp-content/uploads/2015/08/fiesta43254dsiojvs71.jpg");
+  background-position: 72.25% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="5"] .cont_inside {
+  background-image: url("https://2.bp.blogspot.com/-5kehOWIdpM0/WA4Q3gVHzFI/AAAAAAAAUCQ/jrjih_DgEVA3SJ7H_wJAhqAk8tL-gVIfQCLcB/s1600/image%2B%25283%2529.jpeg");
+  background-position: 100% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="6"] .cont_inside {
+  background-image: url("https://comoorganizarlacasa.com/wp-content/uploads/2018/03/15-anos-elegantes-3.jpg");
+  background-position: 100% 50%;
+  background-repeat: no-repeat;
+}
+
+[data-accordion="1.1"] .cont_inside {
+  background-image: url("https://www.marival.com.mx/media/galleries/145/ateringoda32_8f89c812.jpg");
+  background-position: 0 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="2.2"] .cont_inside {
+  background-image: url("https://santocielos.com/wp-content/uploads/2019/02/matrimonio-cristiano-69.jpg");
+  background-position: 27.85% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="3.3"] .cont_inside {
+  background-image: url("https://bodasplata.me/wp-content/uploads/2018/09/decoraci%C3%B3n-bodas-de-plata-aire-libre.jpg");
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="4.4"] .cont_inside {
+  background-image: url("http://masiacasadelmar.com/wp-content/uploads/2015/02/annaeduarda319-1030x686.jpg");
+  background-position: 72.25% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="5.5"] .cont_inside {
+  background-image: url("https://lh3.googleusercontent.com/0CiJgF0CMWy9HFZTrlTAAUaWQHRmlBlt0EhFPOxT6ZLNK1iW15BzUjs_D0TzbrdOEz3QosslwEkvtBUmDNmMalXQRQ=w1200-h800-l90");
+  background-position: 100% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="6.6"] .cont_inside {
+  background-image: url("https://i0.wp.com/www.entremanteles.com/wp-content/uploads/2017/10/bodas-cali-decoracion-bodas-cali-wedding-planners-cali-entremanteles-2.jpg");
+  background-position: 100% 50%;
+  background-repeat: no-repeat;
+}
+
+[data-accordion="1.1.1"] .cont_inside {
+  background-image: url("http://1.bp.blogspot.com/-3mROKAWBL04/VbO7KY5e0zI/AAAAAAAAGAE/Otcl4jmZbr4/s1600/Circus+Birthday+Clowns+Party+People+Events.jpg");
+  background-position: 0 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="2.2.2"] .cont_inside {
+  background-image: url("https://www.alldesigns.com.ve/wp-content/uploads/2018/08/Fiesta-Toy-Story-Decoracion-Mesa-Dulces.jpg");
+  background-position: 27.85% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="3.3.3"] .cont_inside {
+  background-image: url("http://tutusparafiestas.com/wp-content/uploads/2016/10/Lindas-y-creativas-decoraciones-para-fiestas-infantiles-35.jpg");
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="4.4.4"] .cont_inside {
+  background-image: url("https://i.ytimg.com/vi/Y5ROKehonPA/maxresdefault.jpg");
+  background-position: 72.25% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="5.5.5"] .cont_inside {
+  background-image: url("https://www.comidaparafiestas.com.co/images/geleria/nuevas-2016/munecos-para-fiestas-infantiles/munecos-para-fiestas-infantiles-11.jpg");
+  background-position: 100% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="6.6.6"] .cont_inside {
+  background-image: url("https://www.agugueventos.com/imagenes/galerias/decoracion/nueva-15.jpg");
+  background-position: 100% 50%;
+  background-repeat: no-repeat;
+}
+
+[data-accordion="1.1.1.1"] .cont_inside {
+  background-image: url("https://i.ytimg.com/vi/YUkc7Iw1qps/maxresdefault.jpg");
+  background-position: 0 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="2.2.2.2"] .cont_inside {
+  background-image: url("https://colegiomayordelosandes.edu.co/wp-content/uploads/2018/03/primeras-comuniones-cma3-1024x683.jpg");
+  background-position: 27.85% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="3.3.3.3"] .cont_inside {
+  background-image: url("https://www.diariodelhuila.com/uploads/9/Sociales/Mariana.JPG");
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="4.4.4.4"] .cont_inside {
+  background-image: url("http://colegionuevayork.edu.co/wp-content/uploads/2017/06/primeras-comuniones-cny-2017-19.jpg");
+  background-position: 72.25% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="5.5.5.5"] .cont_inside {
+  background-image: url("https://i.ytimg.com/vi/LLl-9fHAqTU/maxresdefault.jpg");
+  background-position: 100% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="6.6.6.6"] .cont_inside {
+  background-image: url("https://colegiomayordelosandes.edu.co/wp-content/uploads/2018/03/primeras-comuniones-cma28-1024x683.jpg");
+  background-position: 100% 50%;
+  background-repeat: no-repeat;
+}
+
+[data-accordion="1.1.1.1.1"] .cont_inside {
+  background-image: url("https://st1.agmeducation.com/media/2014/10/Carlos.Segura.OlivaGrad.jpg");
+  background-position: 0 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="2.2.2.2.2"] .cont_inside {
+  background-image: url("https://wisconsinlcnews.com/wp-content/uploads/2016/06/DSC_0278.jpg");
+  background-position: 27.85% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="3.3.3.3.3"] .cont_inside {
+  background-image: url("https://inevcdn.azureedge.net/cr/2017/03/120550/graduaciones-y-alquileres-chagoz-235213-i-1080w.jpg");
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="4.4.4.4.4"] .cont_inside {
+  background-image: url("http://www.geodomehome.org/images/decoracion-para-graduaciones-youtube__aHR0cHM6Ly9pLnl0aW1nLmNvbS92aS9tTEgwYWRDWG1RVS9tYXhyZXNkZWZhdWx0LmpwZw==.jpg");
+  background-position: 72.25% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="5.5.5.5.5"] .cont_inside {
+  background-image: url("https://www.gestablishment.com/wp-content/uploads/2017/07/graduation-table-decoration-ideas.jpg");
+  background-position: 100% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="6.6.6.6.6"] .cont_inside {
+  background-image: url("https://i.pinimg.com/originals/8a/4d/2f/8a4d2fdae648ea39f8f79b28e727b964.jpg");
+  background-position: 100% 50%;
+  background-repeat: no-repeat;
+}
+
+[data-accordion="1.1.1.1.1.1"] .cont_inside {
+  background-image: url("https://tutusparafiestas.com/wp-content/uploads/2018/02/decoracion-de-bautizos-3.jpg");
+  background-position: 0 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="2.2.2.2.2.2"] .cont_inside {
+  background-image: url("https://evograf.mx/design-evograf/uploads/2017/06/fotografia-video-bautizo-guadalajaraEVOGRAF-4-2-1024x699.jpg");
+  background-position: 27.85% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="3.3.3.3.3.3"] .cont_inside {
+  background-image: url("http://hermesleon.com/wp-content/uploads/2015/03/fotografo-de-bautizos-1.jpg");
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="4.4.4.4.4.4"] .cont_inside {
+  background-image: url("https://pbs.twimg.com/media/DZoqtqDWAAEaYBl.jpg");
+  background-position: 72.25% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="5.5.5.5.5.5"] .cont_inside {
+  background-image: url("https://i.pinimg.com/originals/70/8c/fa/708cfa7fba1478eb5dfd4f5b01ea4ecb.jpg");
+  background-position: 100% 50%;
+  background-repeat: no-repeat;
+}
+[data-accordion="6.6.6.6.6.6"] .cont_inside {
+  background-image: url("https://noviembredulce.es/2301-thickbox_default/cartel-bautizo-erizo.jpg");
+  background-position: 100% 50%;
+  background-repeat: no-repeat;
+}
+
+
+.cont_caption h1 {
+  margin: 0;
+  color: #fff;
+  font-weight: 100;
+  font-size: 3.5rem;
+  white-space: nowrap;
+  text-align: center;
+  line-height: 1;
+  text-shadow: 0 0 10px rgba(0,0,0,0.5);
 }
 
 </style>
